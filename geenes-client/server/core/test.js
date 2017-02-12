88d62a556875cc1d576ece1d6dbc0b00cdb6eb40
gigig987 <gianlucagini@MacBook-Pro-di-Gianluca.local>
2017-02-12 12:39:11 +0000

    saving new project

--- geenes-client/server/core/test.js
+++ geenes-client/server/core/test.js
@@ -35,3 +35,31 @@ function createGenes(n){
 }
 
 exports.createGenes = createGenes;
+
+function createDesign(genes){
+
+      //here I select, based on random genes, the basic rules of the design document
+      var constrastIndex = genes[0];
+      var baseFontSize = geenes.common.randomElement(geenes.typography.baseFontSizes, genes[1] );
+      var baselineM = geenes.common.randomElement(geenes.typography.baseLineM, genes[2] );   
+      var typeScaleRatio = geenes.common.randomElement(geenes.typography.typeScales, genes[3] );
+      
+      var baseFontWeight = geenes.common.randomElement(geenes.typography.fontWeights, genes[4] );
+      var fontWeightContrast = geenes.common.randomElement(geenes.typography.fontWeights, Math.abs((genes[4] -1)));
+
+     // console.log( "g4:"+ genes[4] +" 1-g4:"+ Math.abs((genes[4] -1))) ;
+      //console.log( Math.abs(1 - (typeScaleRatio -1)) * (genes[4] - genes[5]));
+
+
+
+      //Here each style is filled with the proper bho
+      var tBody = geenes.typography.typeSettings(baseFontSize, typeScaleRatio, baselineM , 0,baseFontWeight);
+      var tH2 = geenes.typography.typeSettings(baseFontSize, typeScaleRatio, baselineM , 1,fontWeightContrast);
+      var tH1 = geenes.typography.typeSettings(baseFontSize, typeScaleRatio, baselineM , 2,fontWeightContrast);
+      var tSmall = geenes.typography.typeSettings(baseFontSize, typeScaleRatio, baselineM , -1,baseFontWeight);
+//REACTIVATE WHEN GET DESIGN RULES FUNCTION IS CREATED
+      this.design = {"paragraph": tBody, "heading-1": tH1, "heading-2": tH2, "small": tSmall} ;
+      return this.design;
+}
+
+exports.createDesign = createDesign;
--- geenes-client/server/routes/api.js
+++ geenes-client/server/routes/api.js
@@ -9,68 +9,85 @@ const gen = require('../core/test.js');
 // database
 // const MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
+require('mongoose').Promise = global.Promise;
 var Project = require('../models/project.js');
 
-mongoose.connect('mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASSWORD+'@ds035059.mlab.com:35059/geenes');
+mongoose.connect('mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@ds035059.mlab.com:35059/geenes');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-   router.get('/', (req, res) => {
-    res.send('api works');
-    });
+db.once('open', function () {
+        router.get('/', (req, res) => {
+                res.send('api works');
+        });
 });
 
 // Get all projects
 router.get('/projects', (req, res) => {
-//       db.collection('genes').find({},{'projectName':1, _id: 0})
-//         .toArray((err,results) => {
-//                 if(err)            
-//                 res.status(500).send(error);
-//                 res.status(200).json(results);
-//         })
-        Project.find({},'projectName _id',(err,results) => {
-                if(err)            
+        Project.find({}, 'name _id', (err, results) => {
+                if (err)
                 res.status(500).send(error);
-                res.status(200).json(results);})
+                res.status(200).json(results);
+        })
 });
 
 // Get single project
 router.get('/projects/:id', (req, res) => {
-      db.collection('genes').find({projectName: req.params.id})
-        .toArray((err,results) => {
-                if(err)            
-                res.status(500).send(error);
-                res.status(200).json(results);
-        })
+        db.collection('genes').find({ projectName: req.params.id })
+                .toArray((err, results) => {
+                        if (err)
+                        res.status(500).send(error);
+                        res.status(200).json(results);
+                })
 
 
 });
 
-//create new project
+// create a new project;
+// This will have ONE(1) generation, FIVE(5) specimens,
+//  containing a design document, and an array of TWENTY(20) genes
 router.post('/projects', (req, res) => {
-        var obj = {
-    _id:  mongoose.Types.ObjectId(),            
-    name: 'cscc',//req.body.name,
-    mutationRate: req.body.mRate,
-    generations: [{specimens:[{
+
+//  FOR NOW LET'S  LEAVE THIS CODE IN THE API, THEN MOVE TO APP
+
+        var specimens = []; // array containing all the specimens
+        var design = {}; // object containing all the design rules for the specific specimen
+        var genes = []; //array containing the genes for the specific specimen  
+// 
+//      generate all the data for a single specimen
+// 
+        for (var i = 0; i < 5; i++) { // !HARDCODED(5) number of specimen per project for now
+                genes[i] = gen.createGenes(20); // !HARDCODED(20) number of genes
+                design = gen.createDesign(genes[i]); 
+
+                specimens[i] = {
                         _id: mongoose.Types.ObjectId(),
                         fitness: 1,
-                        dna: { genes: gen.createGenes(20)  }, 
-                        design: {}                
-                        }]}]
-}
-
-console.log(obj.generations[0].specimens[0].dna.genes);
-        new Project(obj).save(err=>{
-                  if (err) {
+                        dna: { genes: genes[i] },
+                        design: { design }
+                }
+        }
+//      
+//      putting together all the data generated before.
+//      models/project.js contains the Schema for this object
+        var obj = {
+                _id: mongoose.Types.ObjectId(),
+                name: req.body.name,
+                mutationRate: req.body.mRate,
+                generations: [{
+                        specimens: specimens
+                }]
+        }
+// ACTUAL CODE TO SAVE THE NEW CREATED PROJECT INTO DB
+        new Project(obj).save(err => {
+                if (err) {
                         console.log(err);
-                        } else {
-                        console.log('meow');
-                        }
+                } else {
+                        console.log('projects saved to database');
+                }
         });
-              
-                
- });
+
+
+});
 
 module.exports = router;
